84e664709490f1616b75924aafe71acb5bf28e96
Eugeneiregi <eugeneregi2019@gmail.com>
2020-04-23 11:52:05 +0300

    encryption

--- user.py
+++ user.py
@@ -1,6 +1,13 @@
+
 class User:
     """
     Class that generates new instances of users
     """
 
     user_list = []
+
+    def __init__(self, first_name, last_name, username):
+    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
