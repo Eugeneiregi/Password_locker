904d9f6a779a68e3b1d6a865aba6b58a4045908a
Eugeneiregi <eugeneregi2019@gmail.com>
2020-04-23 13:06:21 +0300

    First stup self

--- lock_test.py
+++ lock_test.py
@@ -7,3 +7,13 @@ class TestUser(unittest.TestCase):
     Test class for user class
     '''
 
+if __name__== '__main__':
+    unittest.main()
+
+    def setUp(self):
+        '''
+        test to run before each test cases
+        '''
+
+        self.new_credentials = Credentials("Facebook", "1234567")
+        self.new_user = User("Eugene", "peter", "Iregi")
